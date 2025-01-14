24e7a10f7d46bce0ef58d0b96a99db5f86443f7e
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-01-14 16:31:19 +0530

    faq in progress

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+    arrows: false,
+    infinity: true
+});
